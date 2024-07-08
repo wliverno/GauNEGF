Fixed one more unit conversion that was causing g(E) to rapidly change after first SCF iteration

scfE.py
@@ -103,7 +103,7 @@ class NEGFE(NEGF):
         Fock_old = self.F.copy()
         dE, RMSDP, MaxDP = super().PToFock(damping, Edamp)
         self.F, self.locs = getFock(self.bar, self.spin)
-        self.g.setF(self.F)
+        self.g.setF(self.F*har_to_eV)
         
         # Plot integral path and contours
         fig, ax = plt.subplots()