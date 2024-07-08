Fixed unit conversion in NEGFE(), implemented new contour integration algorithm with Fermi broadening

scf2.py
@@ -57,6 +57,7 @@ class NEGF(object):
         self.start_time = time.time()
     
         self.bar = qcb.BinAr(debug=False,lenint=8,inputfile=self.ifile)
+        self.bar.write('debug.baf')
         self.runDFT(chkInit)
 
         # Prepare self.F, Density, self.S, and TF matrices
@@ -88,7 +89,7 @@ class NEGF(object):
     def runDFT(self, chkInit):
         if chkInit:
             try:
-                self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock="DENSITY",chkname=self.chkfile, miscroute=self.otherRoute)
+                self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock=True,chkname=self.chkfile, miscroute=self.otherRoute)
                 print('Checking '+self.chkfile+' for saved data...');
             except:
                 print('Checkpoint not loaded, running full SCF...');
@@ -99,8 +100,8 @@ class NEGF(object):
             
         else:
             print('Using default Harris DFT guess to initialize...')
-            self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock="GUESS",chkname=self.chkfile)
-            self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock=True)
+            self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock="GUESS",chkname=self.chkfile, miscroute=self.otherRoute)
+            self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock=True, miscroute=self.otherRoute)
             print("Done!")
             self.F, self.locs = getFock(self.bar, self.spin)
     
@@ -111,11 +112,8 @@ class NEGF(object):
         self.F = F_ 
     def setVoltage(self, fermi, qV):
         self.fermi = fermi
-        if fermi<self.Emin:
-            self.Emin = fermi-15;
-        if fermi<self.Eminf:
-            self.Emin = fermi-15;
-            self.Eminf = fermi-1e5;
+        self.Emin = fermi-15;
+        self.Eminf = fermi-1e5;
         self.qV = qV
         self.mu1 =  fermi + (qV/2)
         self.mu2 =  fermi - (qV/2)
@@ -224,9 +222,9 @@ class NEGF(object):
         Dense_diff = abs(np.diagonal(self.P) - Dense_old)
         
         ##DEBUG
-        print('DEBUG: Compare Density')
-        print(np.real(np.diag(self.P)[:10]))
-        print(np.real(np.diag(Pback)[:10]))
+        #print('DEBUG: Compare Density')
+        #print(np.real(np.diag(self.P)[:10]))
+        #print(np.real(np.diag(Pback)[:10]))
 
         
         # Apply Damping, store to Gaussian matrix
@@ -240,7 +238,7 @@ class NEGF(object):
         print('Total number of electrons: ', self.nelec)
         
         # Run Gaussian, update SCF Energy
-        self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock="density", miscroute=self.otherRoute)
+        self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock="DENSITY", miscroute=self.otherRoute)
         self.Total_E_Old = self.Total_E.copy()
         self.Total_E  = self.bar.scalar("escf")
         print("SCF energy: ", self.Total_E)
@@ -253,7 +251,7 @@ class NEGF(object):
         print(f'MaxDP: {MaxDP:.2E} | RMSDP: {RMSDP:.2E}')
         return dE, RMSDP, MaxDP
 
-    def SCF(self, conv=1e-5, damping=0.1, maxcycles=100, Edamp=False, plot=True):
+    def SCF(self, conv=1e-5, damping=0.1, maxcycles=100, Edamp=False, plot=False):
         
         Loop = True
         Niter = 0

scfE.py
@@ -3,6 +3,8 @@ from numpy import linalg as LA
 import sys
 import time
 import matplotlib.pyplot as plt
+from PIL import Image
+import os
 
 
 from gauopen import QCOpMat as qco
@@ -40,7 +42,8 @@ class NEGFE(NEGF):
         inds = super().setContacts(contactList[0], contactList[1])
         self.lInd = inds[0]
         self.rInd = inds[1]
-        self.g = surfG(self.F, self.S, inds, tauList, stauList, alphas, aOverlaps, betas, bOverlaps, eps)
+        self.g = surfG(self.F*har_to_eV, self.S, inds, tauList, stauList, alphas, aOverlaps, betas, bOverlaps, eps)
+        self.figures = []
         return inds
         
     def getSigma(self, E):
@@ -71,9 +74,11 @@ class NEGFE(NEGF):
         
         # Density contribution from above self.Emin
         print('Calculating Density for left contact:')
-        P1 = self.g.densityComplex(self.Emin, self.mu1, 0)
+        #P1 = self.g.densityComplex(self.Emin, self.mu1, 0)
+        P1 = self.g.densityComplex(Emin = self.Emin, Emax = self.mu1 + 5*kT, ind=0, mu=self.mu1)
         print('Calculating Density for right contact:')
-        P2 = self.g.densityComplex(self.Emin, self.mu2, 1)
+        #P2 = self.g.densityComplex(self.Emin, self.mu2, 1)
+        P2 = self.g.densityComplex(Emin = self.Emin, Emax = self.mu2 + 5*kT, ind=1, mu=self.mu2)
         
         # Sum them Up.
         P = P1 + P2 + Pw
@@ -87,8 +92,8 @@ class NEGFE(NEGF):
         inds = np.argsort(EList)        
         
         #DEBUG:
-        for pair in zip(occList[inds], EList[inds]):                       
-            print("Energy=", str(pair[1]), ", Occ=", str(pair[0]))
+        #for pair in zip(occList[inds], EList[inds]):                       
+        #    print("Energy=", str(pair[1]), ", Occ=", str(pair[0]))
 
         return EList[inds], occList[inds]
 
@@ -98,8 +103,25 @@ class NEGFE(NEGF):
         Fock_old = self.F.copy()
         dE, RMSDP, MaxDP = super().PToFock(damping, Edamp)
         self.F, self.locs = getFock(self.bar, self.spin)
-        #self.g.setF(self.F)
+        self.g.setF(self.F)
         
+        # Plot integral path and contours
+        fig, ax = plt.subplots()
+        ax.plot(self.g.Egrid[0].real, self.g.Egrid[0].imag, '-r')
+        ax.plot(self.g.poleList[0].real, self.g.poleList[0].imag, 'xr')
+        ax.axvline(self.mu1, c = 'r', ls='-')
+        ax.plot(self.g.Egrid[1].real, self.g.Egrid[1].imag, '--b')
+        ax.plot(self.g.poleList[1].real, self.g.poleList[1].imag,'+b')
+        ax.axvline(self.mu2, c = 'b', ls='--')
+        ax.set_xlabel('Re(Z) eV')
+        ax.set_ylabel('Imag(Z) eV')
+        ax.set_title(f'Frame {len(self.figures)}: RMSDP - {RMSDP:.2E}, MaxDP - {MaxDP:.2E}')
+        lowBnd = self.Emin
+        upBnd = max(self.mu1, self.mu2)+5*kT
+        ax.set_xlim(lowBnd, upBnd)
+        ax.set_ylim(-1, upBnd-lowBnd)
+        self.figures.append(fig)
+
         # Debug:
         #D,V = LA.eig(self.X@(Fock_old*har_to_eV)@self.X) 
         #EListBefore = np.sort(np.array(np.real(D)).flatten())
@@ -107,7 +129,29 @@ class NEGFE(NEGF):
         #EList = np.sort(np.array(np.real(D)).flatten())
         #for pair in zip(EListBefore, EList):                       
         #    print("Energy Before =", str(pair[0]), ", Energy After =", str(pair[1]))
-        
+         
         return dE, RMSDP, MaxDP
 
+    def plotAnimation(self, gif_path='output.gif'):
+        images = []
 
+        for fig in self.figures:
+            # Save the figure to a temporary file
+            fig_path = f'temp_frame_{self.figures.index(fig)}.png'
+            fig.savefig(fig_path)
+            plt.close(fig)
+            
+            # Open the image and append to the list
+            images.append(Image.open(fig_path))
+            
+            # Remove the temporary file
+            os.remove(fig_path)
+
+        # Save all frames as a new or updated GIF
+        if images:
+            images[0].save(gif_path, format='GIF', append_images=images[1:], save_all=True, duration=300, loop=0)
+            print(f'Saved GIF as {gif_path}')
+        else:
+            print('No frames to save.')
+        self.figures = []
+        print('Figures stack cleared!')

surfGreen.py
@@ -37,6 +37,8 @@ class surfG:
         self.S = np.array(Overlap)
         self.X = np.array(fractional_matrix_power(Overlap, -0.5))
         self.indsList = indsList
+        self.poleList = len(indsList)*[np.array([], dtype=complex)]
+        self.Egrid = len(indsList)*[np.array([], dtype=complex)]
         
         # Set Contact Coupling
         if isinstance(taus, int):
@@ -112,7 +114,8 @@ class surfG:
            self.tauList = [self.F[np.ix_(taus[0], taus[1])], self.F[np.ix_(taus[1], taus[0])]]
            self.stauList = [self.S[np.ix_(taus[0], taus[1])], self.S[np.ix_(taus[1], taus[0])]]
     
-    def sigma(self, E, i, conv=1e-5):
+    def sigma(self, E, i, conv=1e-3):
+        E = E.real
         sigma = np.array(np.zeros(np.shape(self.F)), dtype=complex)
         inds = self.indsList[i]
         stau = self.stauList[i]
@@ -122,7 +125,8 @@ class surfG:
         sigma[np.ix_(inds, inds)] += sig
         return sigma
     
-    def sigmaTot(self, E, conv=1e-5):
+    def sigmaTot(self, E, conv=1e-3):
+        E = E.real
         sigma = np.array(np.zeros(np.shape(self.F)), dtype=complex)
         for i, inds in enumerate(self.indsList):
             stau = self.stauList[i]
@@ -134,14 +138,15 @@ class surfG:
     
 
     def denFunc(self, E, ind=-1, mu=-9999):
+        sigTot = self.sigmaTot(E)
         if ind==-1:
-            sig = self.sigmaTot(E)
+            sig = sigTot
         else:
             sig = self.sigma(E, ind)
         Gambar = self.X@(1j*(sig - sig.conj().T))@self.X
         if mu!=-9999:
-            Gambar /= np.exp((E-mu)/kT)+1
-        Fbar = self.X@(self.F + sig)@self.X
+            Gambar /= (np.exp((E-mu)/kT)+1)
+        Fbar = self.X@(self.F + sigTot)@self.X
         D, V = LA.eig(Fbar)
         V = np.array(V, dtype=complex)
         Ga = np.array(np.diag(1/(E-np.conj(D))))
@@ -150,20 +155,23 @@ class surfG:
         Gr = V@Gr@V.conj().T
         return Gr@Gambar@Ga
     
-    def recursiveResidue(self, E, ind=-1, conv=1e-3):
+    def recursiveResidue(self, E, ind, conv=1e-3):
         resid = 1
-        while resid>conv:
-            #print(E, resid)
-            Eprev = E+0.0
-            if ind==-1:
-                sig = self.sigmaTot(E)
-            else:
-                sig = self.sigma(E, ind)
-            Fbar = self.X@(self.F + sig)@self.X
+        counter = 1
+        print(f'RRes starting for E={E}')
+        Eprev = E+0.0
+        while resid>conv and counter<50:
+            Eprev = (E+Eprev)*0.5
+            Fbar = self.X@(self.F + self.sigmaTot(Eprev))@self.X
             D, V = LA.eig(Fbar);
-            Eind = np.argmin(abs(E - D.flatten()))
-            E = np.conj(D.flatten()[Eind])
+            E = D[np.argmin(abs(Eprev - D))].conj()
             resid = abs((Eprev - E)/Eprev)
+            #print(E, resid)
+            counter += 1
+        if counter <50:
+            print(f'RRes Done in {counter} iterations, final E={E}')
+        else:
+            print(f'RRes timed out, resid={resid}, final E={E}')
         return E
 
     def densityGrid(self, Emin, Emax, ind=-1, dE=0.001):
@@ -185,36 +193,45 @@ class surfG:
         theta = np.linspace(0, np.pi, N)
         Egrid = r*np.exp(1j*theta)+center
         #Calculate Residues, use center energy
-        if ind==-1:
-            sig = self.sigmaTot(center)
-        else:
-            sig = self.sigma(center, ind)
-        Fbar = self.X@(self.F + sig)@self.X
+        Fbar = self.X@(self.F + self.sigmaTot(center))@self.X
         Res = np.array(np.zeros(np.shape(self.F)), dtype=complex)
         I = np.array(np.identity(len(Fbar)))
         D, V = LA.eig(Fbar)
-        Gambar = self.X@(1j*(sig - sig.conj().T))@self.X
+        # Find minimum spacing and cut out any values 2*r from center
+        minDist = min([abs(D[i] - D[j]) for i in range(len(D)) for j in range(len(D)) if i>j])
+        D = D[abs(D-center)<(1.5*r)]
+        poleList = D.copy().conj();
+        
+        #Loop through each possible pole and calculate Residue
         for j, E in enumerate(D):
-            if abs(E-center) < r:
-                #print('Residue, E=', E)
-                if recursiveResid:
-                    Enew = self.recursiveResidue(E, ind)
-                    #print('New Residue, E=',Enew)
-                    if ind==-1:
-                        sig = self.sigmaTot(Enew)
-                    else:
-                        sig = self.sigma(Enew, ind)
-                    Fbar = self.X@(self.F + sig)@self.X
-                    Gambar = self.X@(1j*(sig - sig.conj().T))@self.X
-                    if mu!=-9999:
-                        Gambar /= np.exp((Enew-mu)/kT)+1
-                    D, V = LA.eig(Fbar)
+            if recursiveResid:
+                E = self.recursiveResidue(E, j)
+            sigTot = self.sigmaTot(E)
+            if ind==-1:
+                sig = sigTot
+            else:
+                sig = self.sigma(E, ind)
+            Fbar = self.X@(self.F + sigTot)@self.X
+            D, V = LA.eig(Fbar)
+            Gambar = self.X@(1j*(sig - sig.conj().T))@self.X
+            if mu!=-9999:
+                Gambar /= (np.exp((E-mu)/kT)+1)
+            
+            # Final check - ignore if outside of contour or duplicate
+            subPoleList = np.delete(poleList, j)
+            if len(subPoleList) > 0:
+                polDist = min(abs(subPoleList-E.conj()))
+            else:
+                polDist = minDist
+            if abs(E-center) <= r and polDist > minDist/10:
+                print('Residue, E=', poleList[j])
                 Ga = np.array(np.diag(1/(E-np.conj(D))))
                 Ga = V@ Ga @ V.conj().T 
                 Vrow = np.array([V[j,:]])
                 Y = Vrow.T @ Vrow.conj()
-                Res += 2j*np.pi*np.conj(Y@Gambar@Ga) #WHY CONJUGATE???
-                #Res += 2j*np.pi*denFunc(D, V, Gambar, E+1e-9)*(-1e-9)
+                Res += 2j*np.pi*np.conj(Y@Gambar@Ga)
+            #Res += 2j*np.pi*denFunc(D, V, Gambar, E+1e-9)*(-1e-9)
+            poleList[j] = E.conj()
         
         #Integrate along contour
         print('Starting Integration...')
@@ -224,6 +241,10 @@ class surfG:
             dS = Egrid[i]-Egrid[i-1]
             lineInt += self.denFunc(E, ind,mu)*dS
         print('Integration done!')
+        
+        # Store Egrid and poleList for access
+        self.Egrid[ind] = Egrid
+        self.poleList[ind]  = poleList
 
         #Use Residue Theorem
         return (Res-lineInt)/(2*np.pi)