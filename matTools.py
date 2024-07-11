Cleaned up utilities in matTools.py, fixed residue calculation  in surfG()

matTools.py
@@ -38,36 +38,37 @@ kB = 8.617e-5           # eV/Kelvin
 def density(V, D, Gam, Emin, mu):
     Nd = len(V)
     DD = np.array([D for i in range(Nd)]).T
+    
+    #Integral of 1/x is log(x), calculating at lower and upper limit
     logmat = np.array([np.emath.log(1-(mu/D)) for i in range(Nd)]).T
     logmat2 = np.array([np.emath.log(1-(Emin/D)) for i in range(Nd)]).T
 
-
+    #Compute integral, add prefactor
     invmat = 1/(2*np.pi*(DD-DD.conj().T))
     pref2 = logmat - logmat.conj().T
     pref3 = logmat2-logmat2.conj().T
 
     prefactor = np.multiply(invmat,(pref2-pref3))
 
-
+    #Convert Gamma into Fbar eigenbasis, element-wise multiplication
     Vc = LA.inv(V.conj().T)
-
     Gammam = Vc.conj().T@Gam@Vc
-
     prefactor = np.multiply(prefactor,Gammam)
-
+    
+    #Convert back to input basis, return
     den = V@ prefactor @ V.conj().T
-
     return den
 
+# G< function converting to Fbar eigenbasis
 def denFunc(D, V, Gambar, E):
-    V = np.array(V, dtype=complex)
     Ga = np.array(np.diag(1/(E-np.conj(D))))
-    Ga = V*Ga*V.conj().T
+    Ga = V@Ga@V.conj().T
     Gr = np.array(np.diag(1/(E-D)))
-    Gr = V*Gr*V.conj().T
-    return Gr*Gambar*Ga
+    Gr = V@Gr@V.conj().T
+    return Gr@Gambar@Ga
 
-def densityGrid(Fbar, Gambar, Emin, Emax, dE=0.001, T=300):
+# Get density using a (real) energy grid
+def densityGrid(Fbar, Gambar, Emin, mu, dE=0.001, T=300):
     kT = kB*T
     Egrid = np.arange(Emin, Emax+(5*kT), dE)
     I = np.array(np.identity(len(Fbar)))
@@ -82,27 +83,29 @@ def densityGrid(Fbar, Gambar, Emin, Emax, dE=0.001, T=300):
     print('Integration done!')
     return den/(2*np.pi)
 
-def densityComplex(Fbar, Gambar, Emin, Emax, dE=0.001):
+# Get density using a complex contour and applying Residue theorem
+def densityComplex(Fbar, Gambar, Emin, mu, dE=0.001, T=300):
     #Construct circular contour
+    kT = kB*T
+    Emax = Emax+(5*kT)
     center = (Emin+Emax)/2
     r = (Emax-Emin)/2
     N = int((Emax-Emin)/dE)
     theta = np.linspace(0, np.pi, N)
     Egrid = r*np.exp(1j*theta)+center
     
-    #Calculate Residues
-    Res = np.array(np.zeros(np.shape(Fbar)), dtype=complex)
-    I = np.array(np.identity(len(Fbar)))
+    #Calculate Residues (assuming Fbar is energy independent)
+    Res = np.zeros(np.shape(Fbar), dtype=complex)
+    I = np.identity(len(Fbar))
     D, V = LA.eig(Fbar)
-    V = np.array(V, dtype=complex)
     for ind, E in enumerate(D):
         if abs(E-center) < r:
-            #print(E)
+            print('Residue at E=',E)
             Ga = np.array(np.diag(1/(E-np.conj(D))))
-            Ga = V* Ga * V.conj().T 
-            Y = V[:, ind] * V.conj().T[ind,:]
-            Res += 2j*np.pi*np.conj(Y*Gambar*Ga) #WHY CONJUGATE???
-            #Res += 2j*np.pi*denFunc(D, V, Gambar, E+1e-9)*(-1e-9)
+            Ga = V@ Ga @ V.conj().T
+            Vrow= np.array([V[:, ind]])
+            Y = Vrow.T @ Vrow.conj()
+            Res += 2j*np.pi*np.conj(Y@Gambar@Ga)
     
     #Integrate along contour
     print('Starting Integration...')
@@ -113,7 +116,7 @@ def densityComplex(Fbar, Gambar, Emin, Emax, dE=0.001):
         lineInt += denFunc(D, V, Gambar, E)*dS
     print('Integration done!')
     
-    #Use Residue Theorem
+    #Apply Residue Theorem and return
     return (Res-lineInt)/(2*np.pi)
 
 # Form Sigma matrix given self-energy matrix or value (V) and orbital indices (inds)

scf2.py
@@ -106,7 +106,11 @@ class NEGF(object):
             self.F, self.locs = getFock(self.bar, self.spin)
     
     def updateN(self):
-        self.nelec = np.real(np.trace(self.P))
+        nOcc = np.real(np.trace(self.P@self.S))
+        if self.spin == 'r':     #each occupied orbital is an electron pair
+            self.nelec = nOcc * 2
+        else:                   #each orbital is a single electron    
+            self.nelec = nOcc
         return self.nelec
     def setFock(self, F_):
         self.F = F_ 
@@ -188,16 +192,14 @@ class NEGF(object):
         GamBarW2 = X@self.GamW2@X
         Dw,Vw = LA.eig(np.array(FbarW))
         
-        # Calculate Density
+        # Calculate each integral
         #P1 = densityGrid(Fbar, GamBar2, self.Emin, self.mu1)
         #P2 = densityGrid(Fbar, GamBar2, self.Emin, self.mu2)
         P1 = density(V, D, GamBar1, self.Emin, self.mu1)
         P2 = density(V, D, GamBar2, self.Emin, self.mu2)
         Pw = density(Vw, Dw, GamBarW1+GamBarW2, self.Eminf, self.Emin)
         
-        #P1_ = densityGrid(Fbar, GamBar1, self.Emin, self.mu1)
-        #print(np.diag(P1_-P1))
-        
+        # Sum them up 
         P = P1 + P2 + Pw
         
         # Calculate Level Occupation, Lowdin TF,  Return
@@ -233,9 +235,9 @@ class NEGF(object):
             self.P = Pback + 0.1*damping*(self.P - Pback)
         else:
             self.P = Pback + damping*(self.P - Pback)
-        storeDen(self.bar, self.P, self.spin) 
-        self.updateN()
-        print('Total number of electrons: ', self.nelec)
+        storeDen(self.bar, self.P, self.spin)
+        self.updateN() 
+        print(f'Total number of electrons (NEGF): {self.nelec:.2f}')
         
         # Run Gaussian, update SCF Energy
         self.bar.update(model=self.method, basis=self.basis, toutput=self.ofile, dofock="DENSITY", miscroute=self.otherRoute)

scfE.py
@@ -99,28 +99,29 @@ class NEGFE(NEGF):
 
     
     # Use Gaussian to calculate the Density Matrix
-    def PToFock(self, damping, Edamp=False):
+    def PToFock(self, damping, Edamp=False, debug=True):
         Fock_old = self.F.copy()
         dE, RMSDP, MaxDP = super().PToFock(damping, Edamp)
         self.F, self.locs = getFock(self.bar, self.spin)
         self.g.setF(self.F*har_to_eV)
         
-        # Plot integral path and contours
-        fig, ax = plt.subplots()
-        ax.plot(self.g.Egrid[0].real, self.g.Egrid[0].imag, '-r')
-        ax.plot(self.g.poleList[0].real, self.g.poleList[0].imag, 'xr')
-        ax.axvline(self.mu1, c = 'r', ls='-')
-        ax.plot(self.g.Egrid[1].real, self.g.Egrid[1].imag, '--b')
-        ax.plot(self.g.poleList[1].real, self.g.poleList[1].imag,'+b')
-        ax.axvline(self.mu2, c = 'b', ls='--')
-        ax.set_xlabel('Re(Z) eV')
-        ax.set_ylabel('Imag(Z) eV')
-        ax.set_title(f'Frame {len(self.figures)}: RMSDP - {RMSDP:.2E}, MaxDP - {MaxDP:.2E}')
-        lowBnd = self.Emin
-        upBnd = max(self.mu1, self.mu2)+5*kT
-        ax.set_xlim(lowBnd, upBnd)
-        ax.set_ylim(-1, upBnd-lowBnd)
-        self.figures.append(fig)
+        # Plot integral path and poles
+        if debug==True:
+            fig, ax = plt.subplots()
+            ax.plot(self.g.Egrid[0].real, self.g.Egrid[0].imag, '-r')
+            ax.plot(self.g.poleList[0].real, self.g.poleList[0].imag, 'xr')
+            ax.axvline(self.mu1, c = 'r', ls='-')
+            ax.plot(self.g.Egrid[1].real, self.g.Egrid[1].imag, '--b')
+            ax.plot(self.g.poleList[1].real, self.g.poleList[1].imag,'+b')
+            ax.axvline(self.mu2, c = 'b', ls='--')
+            ax.set_xlabel('Re(Z) eV')
+            ax.set_ylabel('Imag(Z) eV')
+            ax.set_title(f'Frame {len(self.figures)}: RMSDP - {RMSDP:.2E}, MaxDP - {MaxDP:.2E}')
+            lowBnd = self.Emin - 5*kT
+            upBnd = max(self.mu1, self.mu2)+5*kT
+            ax.set_xlim(lowBnd, upBnd)
+            ax.set_ylim(-1, upBnd-lowBnd)
+            self.figures.append(fig)
 
         # Debug:
         #D,V = LA.eig(self.X@(Fock_old*har_to_eV)@self.X) 

surfGreen.py
@@ -148,7 +148,6 @@ class surfG:
             Gambar /= (np.exp((E-mu)/kT)+1)
         Fbar = self.X@(self.F + sigTot)@self.X
         D, V = LA.eig(Fbar)
-        V = np.array(V, dtype=complex)
         Ga = np.array(np.diag(1/(E-np.conj(D))))
         Ga = V@Ga@V.conj().T
         Gr = np.array(np.diag(1/(E-D)))
@@ -185,7 +184,7 @@ class surfG:
         print('Integration done!')
         return den/(2*np.pi)
 
-    def densityComplex(self, Emin, Emax, ind=-1, dE=0.1, recursiveResid=True, mu=-9999):
+    def densityComplex(self, Emin, Emax, ind=-1, dE=0.1, recursiveResid=False, mu=-9999):
         #Construct circular contour
         center = (Emin+Emax)/2
         r = (Emax-Emin)/2
@@ -227,10 +226,10 @@ class surfG:
                 print('Residue, E=', poleList[j])
                 Ga = np.array(np.diag(1/(E-np.conj(D))))
                 Ga = V@ Ga @ V.conj().T 
-                Vrow = np.array([V[j,:]])
+                Vrow = np.array([V[:, j]])
                 Y = Vrow.T @ Vrow.conj()
                 Res += 2j*np.pi*np.conj(Y@Gambar@Ga)
-            #Res += 2j*np.pi*denFunc(D, V, Gambar, E+1e-9)*(-1e-9)
+                #Res += 2j*np.pi*denFunc(D, V, Gambar, E+1e-9)*(-1e-9) #For Testing
             poleList[j] = E.conj()
         
         #Integrate along contour