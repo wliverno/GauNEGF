f3f57fe8d3c8fb1d2d25b46fca0a74999c242eb7
Alex Bernson <alex.c.bernson@gmail.com>
2025-01-26 18:36:01 -0800

    Swich to nbsphinx direct Jupyter Notebook processing

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 
 # Mock imports for autodoc
@@ -12,17 +13,22 @@ author = 'William Livernois'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'nbsphinx',
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # HTML output settings
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    'navigation_depth': 4,
+}
+
+
 
 # Napoleon settings for Google style docstrings
 napoleon_google_docstring = True
@@ -51,4 +57,9 @@ mathjax3_config = {
         'inlineMath': [['$', '$'], ['\\(', '\\)']],
         'displayMath': [['$$', '$$'], ['\\[', '\\]']],
     },
-} 
+}
+
+# NBSphinx settings
+nbsphinx_allow_errors = True  # Continue building docs if notebook execution fails
+nbsphinx_execute = 'auto'     # Execute notebooks during docs build
+
