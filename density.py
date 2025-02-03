Fixed densityReal sign error and surfGTest write error

density.py
@@ -399,7 +399,7 @@ def densityReal(F, S, g, Emin, mu, N=100, T=300, parallel=False,
     if showText:
         print('Integration done!')
     
-    return (-1+0j)*np.imag(defInt)/(np.pi)
+    return (1+0j)*np.imag(defInt)/(np.pi)
 
 def densityGrid(F, S, g, mu1, mu2, ind=None, N=100, T=300, parallel=False,
                 numWorkers=None, showText=True):
@@ -672,7 +672,7 @@ def integralFit(F, S, g, mu, Eminf=-1e6, tol=1e-5, maxN=1000):
     while dP>tol and counter<maxN:
         Emin -= 1
         dP = abs(DOSg(F,S,g,Emin))
-        print(Emin, dP)
+        #print(Emin, dP)
         counter += 1
     if counter == maxN:
         print(f'Warning: Emin still not within tolerance (final value = {dP}) after {maxN} energy samples')

surfGTester.py
@@ -4,7 +4,7 @@ from matTools import formSigma
 
 # Class for generating an energy-independent green's function for scfE.py
 class surfGTest:
-    def __init__(self, Fock, Overlap, indsList, sig1=None, sig2=False):
+    def __init__(self, Fock, Overlap, indsList, sig1=None, sig2=None):
         self.F = Fock
         self.S = Overlap
         self.X = fractional_matrix_power(Overlap, -0.5)
@@ -13,7 +13,7 @@ class surfGTest:
         self.sig = [np.array(np.zeros((self.N, self.N)), dtype=complex)]*2
         if sig1 is not None:
             self.sig[0] = formSigma(indsList[0], sig1, self.N, self.S)
-            if isinstance(sig2, bool):
+            if sig2 is None:
                 self.sig[1] = formSigma(indsList[1], sig1, self.N, self.S)
             else:
                 self.sig[1] = formSigma(indsList[1], sig2, self.N, self.S)