1395f800fe5f72838748dbf8e92e7d566f1d6521
William Livernois <willll@uw.edu>
2026-02-24 21:22:40 -0800

    Fixed H_soc for d-orbitals and reduced integration grid + cycles

--- gauNEGF/config.py
+++ gauNEGF/config.py
@@ -20,8 +20,8 @@ FERMI_DEBUG=False                    # Debugging for fermi search functions
 ADAPTIVE_INTEGRATION_TOL = 1e-4     # Adaptive integration tolerance
 N_KT = 10                           # Number of kT for integration limits
 ENERGY_MIN = -1e6                   # eV - lower bound for energy integration
-MAX_CYCLES = 1000                   # Maximum iteration cycles
-MAX_GRID_POINTS = 1000              # Maximum number of grid points
+MAX_CYCLES = 100                   # Maximum iteration cycles
+MAX_GRID_POINTS = 500               # Maximum number of grid points
 
 # SCF Parameters
 SCF_DAMPING = 0.02              # SCF damping parameter
